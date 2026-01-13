5ad187b5640be37eb3ed00e80fa0bd0855341b54
Divya P <divya@links4engg.com>
2026-01-13 13:54:33 +0530

    initial setup of react

---
+++ frontend/youTube-frontend/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from "./components/Login.jsx"
+import Register from "./components/Register.jsx"
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
