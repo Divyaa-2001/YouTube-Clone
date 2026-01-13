9378e71e4e484d609581966371aefd39717d7de6
Divya P <divya@links4engg.com>
2026-01-13 18:54:59 +0530

    video api schema

---
+++ backend/models/Video.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  videoUrl: String,
+  thumbnailUrl: String,
+  channelName: String,
+  views: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  category: String,
+  uploadDate: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Video", videoSchema);
