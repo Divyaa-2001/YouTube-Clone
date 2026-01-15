37bb3742f3f14fc128309706ea318e70e34cdbe3
Divya P <divya@links4engg.com>
2026-01-16 03:13:43 +0530

    channel scema

--- backend/models/Channel.model.js
+++ backend/models/Channel.model.js
@@ -4,8 +4,7 @@ const channelSchema = new mongoose.Schema({
   channelName: String,
   owner: String,
   description: String,
-  banner: String,
-  subscribers: { type: Number, default: 0 }
+  banner: String
 });
 
 export default mongoose.model("Channel", channelSchema);
