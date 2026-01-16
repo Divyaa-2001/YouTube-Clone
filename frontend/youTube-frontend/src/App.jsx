733a76c6fc3478c43debac5d10d03d8041144b15
Divya P <divya@links4engg.com>
2026-01-16 06:46:39 +0530

    login issues fixed

--- frontend/youTube-frontend/src/App.jsx
+++ frontend/youTube-frontend/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [videos, setVideos] = useState([]);
 
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+
   // Fetch all videos once
   useEffect(() => {
     fetch("http://localhost:8000/videos")
@@ -14,21 +18,19 @@ function App() {
       .then(data => setVideos(data));
   }, []);
 
-  const user = JSON.parse(localStorage.getItem("user"));
-
   return (
     <>
       <Header
         toggleSidebar={() => setIsOpen(!isOpen)}
         user={user}
-        setUser={() => {}}
+        setUser={setUser}
         setVideos={setVideos}
       />
 
       <Sidebar isOpen={isOpen} closeSidebar={() => setIsOpen(false)} />
 
       <main className={`pt-16 px-6 ${isOpen ? "md:ml-60" : "md:ml-20"}`}>
-        <Outlet context={{ videos, setVideos }} />
+        <Outlet context={{ videos, setVideos, setUser }} />
       </main>
     </>
   );
